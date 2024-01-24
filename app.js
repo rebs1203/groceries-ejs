e2584aba97ba0c89e91817ffe646da101feae637
rebecavg <rebecavgallo@hotmail.com>
2024-01-24 14:24:41 -0500

    1st commit for this lesson

--- app.js
+++ app.js
@@ -1,87 +1,109 @@
-const flash = require('connect-flash/lib/flash');
 const express = require('express');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const flash = require('connect-flash');
+const bodyParser = require('body-parser');
+const csrf = require('host-csrf');
+const cookieParser = require('cookie-parser')
+const passport = require('passport');
+const mongoose = require('mongoose');
+const path = require('path');
+const passportInit = require('./passport/passportInit');
+
 require('express-async-errors');
+require('dotenv').config();
 
-const app = express()
+const app = express();
 
-app.set('view engine', 'ejs')
 
-app.use(require('body-parser').urlencoded({ extended: true}))
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.SESSION_SECRET));
+let csrf_development_mode = true;
 
-require('dotenv').config()
-const session = require('express-session')
-const MongoDBStore = require('connect-mongodb-session')(session)
-const url = process.env.MONGO_URI
 
+const url = process.env.MONGO_URI;
 const store = new MongoDBStore({
-    uri: url, 
-    collection: "MySessions"
-})
+    uri: url,
+    collection: 'MySessions',
+});
 
 store.on('error', function (error) {
-    console.log(error)
-})
+    console.log(error);
+});
 
-const sessionParms = {
+const sessionParams = {
     secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: true,
     store: store,
-    cookie: { secure: false, sameSite: "strict" }
-}
+    cookie: { secure: false, sameSite: 'strict' },
+};
 
 if (app.get('env') === 'production') {
-    app.set('trust proxy', 1)
-    sessionParms.cookie.secure = true 
+    csrf_development_mode = false
+    app.set('trust proxy', 1);
+    sessionParams.cookie.secure = true;
 }
 
-app.use(session(sessionParms))
+const csrf_options = {
+    protected_operations: ["PATCH"],
+    protected_content_types: ["application/json"],
+    development_mode: csrf_development_mode,
+  };
+
+app.use(csrf(csrf_options));
+
+app.use(session(sessionParams));
+passportInit();
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+const secretWordRouter = require("./routes/secretWord");
+const auth = require("./middleware/auth");
+app.use("/secretWord", auth, secretWordRouter);
+
+app.use(require('./middleware/storeLocals'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 
-app.get('/secretWord', (req, res) => {
-    if (!req.session.secretWord) {
-        req.session.secretWord = 'syzygy'
-    }
 
-    res.locals.info = req.flash('info')
-    res.locals.errors = req.flash('error')
-    res.render( 'secretWord', { secretWord: req.session.secretWord })
-})
+app.use('/sessions', require('./routes/sessionRoutes'));
 
-app.use(require("connect-flash")())
 
-app.post('/secretWord', (req, res) => {
-    
-    if (req.body.secretWord.toUpperCase()[0] === "P") {
-        req.flash('error', 'that word won`t work!')
-        req.flash('error', 'you can`t use words that starts with P!')
-    } else {
-        req.session.secretWord = req.body.secretWord
-        req.flash('info', 'the secret word has changed')
-    }
-    res.redirect('/secretWord')
-})
+
+
 
 app.use((req, res) => {
-    res.status(404).send(`The page (${req.url}) was not found`)
-})
+    res.status(404).send(`The page (${req.url}) was not found`);
+    console.log(err);
+});
 
 app.use((err, req, res, next) => {
-    res.status(500).send(err.message)
-    console.log(err)
-})
+    res.status(500).send(err.message);
+    console.log(err);
+});
 
-const port = process.env.PORT || 3000
+
+const port = process.env.PORT || 3000;
 
 const start = async () => {
     try {
+        await mongoose.connect(url);
+        console.log('Connected to MongoDB');
         app.listen(port, () => {
-            console.log(`Server is listening on port ${port}`)
-        })
+            console.log(`Server is listening on port ${port}`);
+        });
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-}
+};
 
-start()
+start();
+
 
--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -28,12 +27,7 @@ UserSchema.pre('save', async function() {
     this.password = await bcrypt.hash(this.password, salt)
 })
 
-UserSchema.methods.createJWT = function () {
-
-    return jwt.sign({userId: this._id, name: this.name}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_LIFETIME})
-}
-
-UserSchema.methods.comparePasswords =  async function (candidatePassword) {
+UserSchema.methods.comparePassword =  async function (candidatePassword) {
     
     const isMatch = await bcrypt.compare(candidatePassword, this.password)
     return isMatch
